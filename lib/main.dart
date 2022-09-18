49b9ae0b7d90f8d0372ce6727fb7034b093afb29
DELL <fatmatarek822>
2022-09-18 23:51:28 +0200

    task 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:odc_project/modules/login.dart';
+import 'package:odc_project/modules/new.dart';
+import 'package:odc_project/modules/register.dart';
 import 'package:odc_project/modules/setting.dart';
+import 'package:odc_project/modules/splash.dart';
+import 'package:odc_project/modules/support.dart';
 import 'modules/section.dart';
 
 void main() {
@@ -15,7 +20,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SectionScreen(),
+      home: SplashScreen(),
     );
   }
 }
+
+
+
+
